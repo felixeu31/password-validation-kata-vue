5a3653c46d50a53a428e97fe8cce43e4d0b2fef7
Félix Díez Arias <fdiez@tunstall.es>
2023-08-04 15:34:32 +0200

    CHORE: app clean up

--- password-validator/src/App.vue
+++ password-validator/src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import PasswordValidatorForm from './components/PasswordValidatorForm.vue';
 </script>
 
@@ -9,12 +7,11 @@ import PasswordValidatorForm from './components/PasswordValidatorForm.vue';
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+      <PasswordValidatorForm/>
   </main>
 </template>
 
