2e2923071f0d7fa687362b8bba3c73eb8d50e32c
Félix Díez Arias <fdiez@tunstall.es>
2023-08-05 08:44:32 +0200

    RED: should render a user input

--- password-validator/tests/components/PasswordValidatorForm.spec.ts
+++ password-validator/tests/components/PasswordValidatorForm.spec.ts
@@ -3,8 +3,13 @@ import { render } from '@testing-library/vue';
 import PasswordValidatorForm from '@/components/PasswordValidatorForm.vue';
 
 describe('Password validator form', () => {
-  it('renders form title', () => {
+  it('should render form title', () => {
     const { queryByText } = render(PasswordValidatorForm)    
     expect(queryByText('Password Validator')).not.toBeNull()
   })
+
+  it('should render a user input', () => {
+    const { queryByPlaceholderText } = render(PasswordValidatorForm)    
+    expect(queryByPlaceholderText('Introduce tu contraseña')).not.toBeNull()
+  });
 })
